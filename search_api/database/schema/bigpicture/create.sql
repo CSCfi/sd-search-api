27ddb03ab96e89c6b02f1c4143ecfef0512162e8
Rasko Leinonen <raskolei@csc.fi>
2026-06-08 08:59:41 +0300

    Removed unnecessary indexes from Postgres database. Search is planned to happen only through OpenSearch.

--- search_api/database/schema/bigpicture/create.sql
+++ search_api/database/schema/bigpicture/create.sql
@@ -5,23 +5,13 @@ CREATE TABLE bp_image (
     dataset_short_name TEXT,
     dataset_title TEXT,
     dataset_description TEXT,
-    dataset_short_name_tsv tsvector GENERATED ALWAYS AS (to_tsvector('english', dataset_short_name)) STORED,
-    dataset_title_tsv tsvector GENERATED ALWAYS AS (to_tsvector('english', dataset_title)) STORED,
-    dataset_description_tsv tsvector GENERATED ALWAYS AS (to_tsvector('english', dataset_description)) STORED,
     blocks JSONB, -- block related search fields
     stains JSONB, -- staining related search fields
     search_sync BOOLEAN NOT NULL DEFAULT false,
     search_sync_date timestamptz
 );
 
-----------------------------------------------------
--- Indexes
-----------------------------------------------------
+CREATE INDEX idx_bp_image_dataset_id ON bp_image (dataset_id);
 
-CREATE INDEX idx_bp_image_dataset_short_name_tsv ON bp_image USING GIN (dataset_short_name_tsv);
-CREATE INDEX idx_bp_image_dataset_title_tsv ON bp_image USING GIN (dataset_title_tsv);
-CREATE INDEX idx_bp_image_dataset_description_tsv ON bp_image USING GIN (dataset_description_tsv);
-CREATE INDEX idx_bp_image_blocks ON bp_image USING GIN (blocks jsonb_path_ops); -- Only supports @> (containment) queries.
-CREATE INDEX idx_bp_image_stains ON bp_image USING GIN (stains jsonb_path_ops); -- Only supports @> (containment) queries.
-
-CREATE INDEX idx_bp_image_search_sync ON bp_image (search_sync);
+-- Partial index to keep it smaller.
+CREATE INDEX idx_bp_image_search_sync ON bp_image (image_id) WHERE search_sync = false;
